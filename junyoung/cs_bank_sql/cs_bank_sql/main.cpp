f021a5f7635f816ac887ca940db78a43ca182794
사용자 junyoung011019 <kidjjy5@naver.com>
2023-11-20 23:07:38 +0900

    231120

--- junyoung/cs_bank_sql/cs_bank_sql/main.cpp
+++ junyoung/cs_bank_sql/cs_bank_sql/main.cpp
@@ -7,12 +7,45 @@ MYSQL_RES* Result;        //쿼리 성공시 결과를 담는 구조체 포인터
 MYSQL_ROW Row;            //쿼리 성공시 결과로 나온 행의 정보를 담는 구조체
 int Stat;                 //쿼리 요청 후 결과 (성공, 실패)
 
-int g_end = 0;
+int EndCode = 0;
+
 
 class Login {
+public:
+    string Input_ID, Input_PW;
+    int missing = 0, error = 0;
+    int MemberNo = 0;
 protected:
     void login() {}
 };
+
+void Login::login() {
+    system("cls");
+    cout << "---------------로그인---------------" << "\n";
+    while (1) {
+        cout << "아이디를 입력하세요. >";
+        cin >> Input_ID;
+        cout << "비밀번호를 입력하세요. >";
+        cin >> Input_PW;
+        //아이디 비밀번호를 sql에서 확인하는 구문
+        if (1) {
+            cout << "환영합니다!";
+                //MemberNo 변수에 사용자의 키 값을 삽입
+            break;
+        }
+        else {
+            missing += 1;
+            cout << "비밀번호가 옳지않습니다.";
+            cout << "현재 비밀번호" << missing << "회 오류입니다.";
+            if missing < 5{
+                error = 1;
+                break;
+            }
+            cout << "로그인 5회 실패시 로그인이 제한됩니다.";
+        }
+    }
+
+}
 
 class SignUp {
 public:
@@ -23,7 +56,7 @@ protected:
     void signup();
 };
 
-void SignUp::signup(){
+void SignUp::signup() {
     system("cls");
     cout << "---------------회원가입---------------" << "\n";
     cout << "이름을 입력하세요. (현재는 영문만 가능) >";
@@ -32,7 +65,7 @@ void SignUp::signup(){
         cout << "사용하실 아이디를 입력하세요. (영문 2자)";
         cin >> ID;
         char idcheck[] = "SELECT ID FROM customer_table";
-        cout << idcheck<<endl;
+        cout << idcheck << endl;
         if (idcheck[0] == NULL) {
             break;
         }
@@ -52,24 +85,94 @@ void SignUp::signup(){
         break;
     }
     cout << "고객님의 계좌 번호는" << PhoneNumber << "0 입니다!\n";
-    AcccountNumber= PhoneNumber;
+    AcccountNumber = PhoneNumber;
     stringstream AcccountNumber;
     cout << "가입을 진심으로 환영합니다!!!\n";
     "INSERT INTO `cs_bank`.`customer_table` (`ID`, `Name`, `Phone`, `Password`, `ACCOUNTNUMBER`) VALUES(ID, NAME, PhoneNumber, PW, AcccountNumber)";
     int a;
     cin >> a;
-
-    
 }
 
-class Start: public Login, public SignUp{
+class Start : public Login, public SignUp {
 public:
-    void start();
     int StartMenuNum = 0;
+    void start();
 };
 
-void Start::start(){
-    
+class User : public Start {
+public:
+    int UserMenuNum = 0;
+    void UserFunction();
+    //유저의 정보를 받아오는 내용
+    string UserName;
+
+
+    deposit();
+    withdraw();
+    transfer();
+    AccountCheck();
+    TransferCheck(); break;
+}
+
+void User::deposit() {
+    int InputDeposit;
+    cout << "입금 하실 금액을 입력하세요.";
+    cin >> InputDeposit;
+
+}
+
+void User::withdraw() {
+
+}
+
+void User::transfer() {
+
+}
+
+void User::AccountCheck() {
+
+}
+
+void User::TransferCheck() {
+
+}
+
+void User::UserFunction() {
+    system("cls");
+    cout << "---------------환영합니다!" << UserName << "님!---------------" << "\n";
+    cout << "1. 예금 입금" << endl;
+    cout << "2. 예금 출금" << endl;
+    cout << "3. 계좌 이체" << endl;
+    cout << "4. 계좌 정보 조회" << endl;
+    cout << "5. 이체 내역 확인" << endl;
+    cout << "6. 로그아웃" << endl;
+    cout << "7. 프로그램 종료" << endl
+        cin >> UserMenuNum;
+    switch (UserMenuNum) {
+    case 1:
+        deposit();  break;
+    case 2:
+        withdraw();  break;
+    case 3:
+        transfer(); break;
+    case 4:
+        AccountCheck(); break;
+    case 5:
+        TransferCheck(); break;
+    case 6:
+        MemberNo = 0;
+        break;
+    case 7:
+        EndCode = 1; break;
+    default:
+        system("cls");
+        cout << "잘못된 입력입니다. 다시 입력해주세요.\n";
+        UserFunction();
+    }
+}
+
+void Start::start() {
+
     cout << "------------------------------------" << "\n";
     cout << "1. 회원가입" << endl;
     cout << "2. 로그인" << endl;
@@ -82,11 +185,21 @@ void Start::start(){
     case 2:
         login();  break;
     case 3:
-        g_end = 1; break;
+        EndCode = 1; break;
     default:
         system("cls");
         cout << "잘못된 입력입니다. 다시 입력해주세요.\n";
         start();
+    }
+    if (error = 1) {
+        cout << "에러 코드 1";
+        cout << "로그인에 5회 실패했습니다.";
+        cout << "프로그램이 종료됩니다";
+        EndCode = 1; break;
+    }
+    if (MemberNo != 0) {
+        //로그인 완료
+        break;
     }
 }
 
@@ -94,7 +207,7 @@ int main()
 {
     //////////////////////////////////////MYSQL 연결//////////////////////////////////////
 
-    
+
 
     mysql_init(&Conn);         //mysql 정보 초기화
 
@@ -130,9 +243,20 @@ int main()
         system("cls");
         cout << " =========================**cs 제일은행에 오신 걸 환영합니다**=============================\n\n";
         Start customer;
-        customer.start();
-        if (g_end == 1) {
+        if (customer.MemberNo == 0) {
+            customer.start();
+
+        }
+        else {
+            User user;
+            user.UserFunction();
+        }
+        if (EndCode == 1) {
+            cout << "프로그램이 종료 됩니다!";
             return 0;
         }
+
+
+
     }
 }
